407ff8b53bf92e3d64ac4ba00a5063115e1320fa
BeBeBecon <katsube0611@gmail.com>
2025-10-07 15:52:01 +0900

    Initial Commit

---
+++ AISmartActionApp/AISmartActionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AISmartActionAppApp.swift
+//  AISmartActionApp
+//
+//  Created by Taku.K on 2025/10/07.
+//
+
+import SwiftUI
+
+@main
+struct AISmartActionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AISmartActionAppTests/AISmartActionAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AISmartActionAppTests.swift
+//  AISmartActionAppTests
+//
+//  Created by Taku.K on 2025/10/07.
+//
+
+import Testing
+@testable import AISmartActionApp
+
+struct AISmartActionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
